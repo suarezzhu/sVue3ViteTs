eb70c2f11f18273b26a7af749c80c10366e010d6
suarezzhu <zxc2511836601@gmail.com>
2023-01-29 10:16:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+####  🌈介绍
+
+这是基于开源vue-next-admin框架学习和修改的
+基于 vue3.x + CompositionAPI setup 语法糖 + typescript + vite + element plus + vue-router-next + pinia 技术
+
+
+
+####   📅开始时间
+2023.1.28 15:10
+##### 初始化  npm create vite@latest vite0 --template vue-ts
+ 
+
+
+
+
---
+++ vite.config.ts
@@ -0,0 +1,74 @@
+import { defineConfig,loadEnv,ConfigEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import {resolve}from 'path'
+import vueSetupExtend from 'vite-plugin-vue-setup-extend'; // <script lang='ts' setup name='demo'>  该插件用来在setup语法糖中 定义组件名name
+
+//将相对路径解析为绝对路径 路径转换函数
+const pathResolve=(dir:string)=>{
+  return resolve(__dirname,'.',dir)  //__dirname：用来动态获取当前文件模块所属目录的绝对路径  __filename：用来动态获取当前文件的绝对路径 reslove作用是拼接参数
+}
+
+
+// 别名
+const alias: Record<string, string> = {
+  '/@': pathResolve('./src/'),
+  'vue-i18n': 'vue-i18n/dist/vue-i18n.cjs.js',
+};
+
+
+// 配置
+const viteConfig=defineConfig((mode:ConfigEnv)=>{
+  const env=loadEnv(mode.mode,process.cwd());
+  return{
+    plugins:[vue(),vueSetupExtend()],//插件
+    root:process.cwd(), //  项目根目录（index.html 文件所在的位置）
+    resolve:{alias},  // 定义文件路径别名，当使用文件系统路径的别名时，请始终使用绝对路径，否则无法解析
+    base:mode.command==='serve'?'./' :env.VITE_PUBLIC_PATH,  //开发或生产环境服务的公共基础路径
+    optimizeDeps: {
+      include: ['element-plus/lib/locale/lang/zh-cn', 'element-plus/lib/locale/lang/en', 'element-plus/lib/locale/lang/zh-tw'],
+      //首次执行 vite 时，服务启动后会对 node_modules 模块和配置 optimizeDeps 的目标进行预构建  预构建的模块
+    },
+
+    // 开发代理模式
+    server: {
+      host: '0.0.0.0',
+      port: env.VITE_PORT as unknown as number,
+      open: env.VITE_OPEN,
+      hmr: true,
+      proxy: {
+        '/gitee': {
+          target: 'https://gitee.com',
+          ws: true,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/gitee/, ''),
+        },
+      },
+    },
+
+    // 打包配置
+    build: {
+      outDir: 'dist', // 指定输出路径（相对于 项目根目录)
+      chunkSizeWarningLimit: 1500, // 规定触发警告的 chunk(文件块) 大小
+      rollupOptions: { // 自定义底层的 Rollup 打包配置
+        output: {
+          entryFileNames: `assets/[name].[hash].js`,
+          chunkFileNames: `assets/[name].[hash].js`,
+          assetFileNames: `assets/[name].[hash].[ext]`,
+          compact: true,
+          manualChunks: {
+            vue: ['vue', 'vue-router', 'pinia'],
+            echarts: ['echarts'],
+          },
+        },
+      },
+    },
+    css: { preprocessorOptions: { css: { charset: false } } },
+    define: {
+      __VUE_I18N_LEGACY_API__: JSON.stringify(false),
+      __VUE_I18N_FULL_INSTALL__: JSON.stringify(false),
+      __INTLIFY_PROD_DEVTOOLS__: JSON.stringify(false),
+      __VERSION__: JSON.stringify(process.env.npm_package_version),
+    }
+  }
+})
+export default viteConfig;
